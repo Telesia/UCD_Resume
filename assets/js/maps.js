1207237918056ca5994a4c3f77b2a9d44011585f
Telesia <sarahtelesia@gmail.com>
2020-11-09 19:51:09 +0000

    Added Google Maps with Markers

---
+++ assets/js/maps.js
@@ -0,0 +1,31 @@
+//This whole segment of code is taken from the google maps marker page https://developers.google.com/maps/documentation/javascript/marker-clustering 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var locations = [
+    {lat: 40.785091, lng: -73.968285 },
+    {lat: 41.084045, lng: -73.87425 },
+    {lat: 40.754932, lng: -73.984016 }
+];
+
+var markers = locations.map(function(location, i) {
+    return new google.maps.Marker( {
+        position: location,
+        label: labels[i % labels.length]
+
+    });
+});
+
+new MarkerClusterer(map, markers, {
+    imagePath:
+    "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+});
+}
